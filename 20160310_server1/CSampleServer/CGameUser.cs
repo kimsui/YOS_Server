3dab5018e9c9e48bc6f15feb87f85c4ce399cbfe
김수이 <k1msu4.com@gmail.com>
2016-03-10 17:37:14 +0900

    0310-DB,Server,Client 패킷크기가 Max1024로 한정됨

---
+++ 20160310_server1/CSampleServer/CGameUser.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreeNet;
+
+
+namespace CSampleServer
+{
+	using GameServer;
+
+	/// <summary>
+	/// 하나의 session객체를 나타낸다.
+	/// </summary>
+	class CGameUser : IPeer
+	{
+		CUserToken token;
+
+		public CGameUser(CUserToken token)
+		{
+			this.token = token;
+			this.token.set_peer(this);
+		}
+
+		void IPeer.on_message(Const<byte[]> buffer)
+		{
+			// ex)
+			CPacket msg = new CPacket(buffer.Value, this);
+			PROTOCOL protocol = (PROTOCOL)msg.pop_protocol_id();
+			Console.WriteLine("------------------------------------------------------");
+			Console.WriteLine("protocol id " + protocol);
+			switch (protocol)
+			{
+				case PROTOCOL.CHAT_MSG_REQ:
+					{
+						string text = msg.pop_string();
+						Console.WriteLine(string.Format("INPUT1 COMMAND :  {0}", text));
+
+                        ///이곳에 디비에 접근할 수 있는 코드를 넣어야함
+                        ///메쏘드로 따로 구현을 해야겟다
+                        ///odpconn.cs
+
+                        SAccessDB.odpconn(text);
+
+
+                        /// 아래 코드는 뭘까... 다시 send를 하나??
+                        /// 클라이언트한테 응답을 주는가 보다.
+                        /// 응답 코드 맞음.. 확인함..이 샘플 서버는 에코 서버임..
+						CPacket response = CPacket.create((short)PROTOCOL.CHAT_MSG_ACK);
+                        Console.WriteLine(SAccessDB.makexml());
+
+                        /// xml string의 길이는 미리 알 수 없음으로
+                        /// 1024 단위로 짤라서 보내야 함
+                        /// while문을 만들자...ㅎ 
+                        /// 세션이 연결되어 있으니.. 상관없지 않으려나? 
+                        /// 
+						response.push(SAccessDB.makexml());
+						send(response);
+					}
+					break;
+			}
+		}
+
+		void IPeer.on_removed()
+		{
+			Console.WriteLine("The client disconnected.");
+
+			Program.remove_user(this);
+		}
+
+		public void send(CPacket msg)
+		{
+			this.token.send(msg);
+		}
+
+		void IPeer.disconnect()
+		{
+			this.token.socket.Disconnect(false);
+		}
+
+		void IPeer.process_user_operation(CPacket msg)
+		{
+		}
+	}
+}
---
+++ 20160310_server1/CSampleServer/SAccessDB.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
+using System.IO;
+
+namespace CSampleServer
+{
+    class SAccessDB
+    {
+        static DataTable dt;
+        static DataTable dt2;
+        static DataSet ds;
+        static DataTable CloneDT;
+        static StringBuilder sb;
+        static StringWriter stream;
+
+        static public void odpconn(string strcmd)
+        {
+            string strConn = "User Id=scott;Password=tiger;Data Source=ORCL";
+
+            OracleDataAdapter oraDA = new OracleDataAdapter(strcmd, strConn);
+            dt = new DataTable();
+            oraDA.Fill(dt);
+        }
+        /// <summary>
+        /// 전송할 dt(sting) 만들기
+        ///dt 테이블은 dataset 으로 변환한 후에 xml에 데이터를 저장한후
+        ///xml.string()으로 다시 response를 해줌.
+        /// </summary>
+        /// <param name="dt"></param>
+        /// dt 를 변수로 넣고 싶은데.. 메쏘드를 두개로 나누는 것이 나을지
+        /// 아니면 public static 변수로 두고 같이 쉐어하는 것이 나을지 의논 ..ㅜㅜ
+        static public string makexml()
+        {
+            ds = new DataSet("XMLTABLE");
+
+            CloneDT = dt.Copy();
+            CloneDT.TableName = "XMLTABLE";
+            ds.Tables.Add(CloneDT);
+
+            sb = new StringBuilder();
+            stream = new StringWriter(sb);
+            ds.WriteXml(stream, XmlWriteMode.WriteSchema);
+
+            return stream.ToString();
+        }
+    }
+}
